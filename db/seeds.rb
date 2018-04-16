af70c9fa75ca3ad12abf527224d93ff247bd2d50
classicalliu <classicalliu@gmail.com>
2018-04-16 16:21:06 +0800

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,36 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-terry = Author.create(name: 'Terry Tai', email: 'poshboytl@gmail.com', avatar: 'http://en.gravatar.com/userimage/2432070/00b9a1609df2774d2fdbc03b49ad8546.jpeg', bio: 'Shooting Man', password: 'asdfghjkl')
+terry = Author.create!(
+  name: 'Terry Tai',
+  email: 'poshboytl@gmail.com',
+  avatar: 'http://en.gravatar.com/userimage/2432070/00b9a1609df2774d2fdbc03b49ad8546.jpeg',
+  bio: 'Shooting Man',
+  password: '111'
+)
 
-chenglu = Author.create(name: 'Chenglu She', email: 'smallbanglouis@gmail.com', avatar: 'https://avatars1.githubusercontent.com/u/2050207', bio: 'bang the world', password: '123321')
+chenglu = Author.create!(
+  name: 'Chenglu She',
+  email: 'smallbanglouis@gmail.com',
+  avatar: 'https://avatars1.githubusercontent.com/u/2050207',
+  bio: 'bang the world',
+  password: '111'
+)
+
+content = "
+# Header
+## sub
+
+article content
+
+"
+
+# generate posts
+50.times do |index|
+  terry.posts.create!(
+    title: "Article #{index + 1}",
+    content: content,
+    tag_string: "tag1, tag2, tag3, tag4, tag5, tag6, tag7",
+    published: true
+  )
+end
