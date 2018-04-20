560aadca0b51dc6cd90f045c6bc8a75559bda757
classicalliu <classicalliu@gmail.com>
2018-04-20 15:37:53 +0800

    invite email page

--- app/javascript/src/images/index.js
+++ app/javascript/src/images/index.js
@@ -3,5 +3,6 @@ import './login_bg.svg';
 import './invite_bg.svg';
 import './signup_bg.svg';
 import './forget_password_bg.svg';
+import './logo.png';
 
 
--- test/mailers/previews/invitation_mailer_preview.rb
+++ test/mailers/previews/invitation_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/invitation_mailer
 class InvitationMailerPreview < ActionMailer::Preview
-
+  def invite_email
+    InvitationMailer.invite_email(Invitation.first)
+  end
 end
