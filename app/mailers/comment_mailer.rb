bcf1e5a953095a56b074aeb6dda42bc1bcf927e2
classicalliu <classicalliu@gmail.com>
2018-04-20 16:52:19 +0800

    comment email and text pages

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -2,11 +2,15 @@ class CommentMailer < ApplicationMailer
   helper PostsHelper
 
   def notify_author(post)
+    set_logo_url
+
     @post = post
     mail to: post.author.email, subject: t("comment_mailer.notify_author.subject")
   end
 
   def notify_parent(comment)
+    set_logo_url
+
     @comment = comment
     parent = comment.parent
 
--- test/mailers/previews/comment_mailer_preview.rb
+++ test/mailers/previews/comment_mailer_preview.rb
@@ -1,4 +1,14 @@
 # Preview all emails at http://localhost:3000/rails/mailers/comment_mailer
 class CommentMailerPreview < ActionMailer::Preview
 
+  def notify_author
+    CommentMailer.notify_author(Post.first)
+  end
+
+  def notify_parent
+    comment = Comment.first
+    comment.parent = Comment.last
+    CommentMailer.notify_parent(comment)
+  end
+
 end
