80e8ac72e5cb2ff9f684c5c0045a7d69ae7ff598
classicalliu <classicalliu@gmail.com>
2018-04-23 18:52:28 +0800

    fix locale flash bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   def default_url_options
-    I18n.locale = params[:locale] if I18n.available_locales.include?(params[:locale]&.to_sym)
+    I18n.locale = I18n.available_locales.include?(params[:locale]&.to_sym) ? params[:locale] : I18n.default_locale
     return { locale: I18n.locale } if I18n.locale != I18n.default_locale
     {}
   end
