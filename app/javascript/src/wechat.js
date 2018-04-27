7b22942837b4f6b4fab969150c4367f210d65ca9
classicalliu <classicalliu@gmail.com>
2018-04-27 16:16:01 +0800

    fix modal bug

--- app/javascript/src/wechat.js
+++ app/javascript/src/wechat.js
@@ -1,6 +1,9 @@
 import QRCode from "qrcode";
 
 $(document).on('turbolinks:load', () => {
+  // for fix bug of modal behind of backdrop
+  $('.modal').on('show.bs.modal', function (e) { $('.modal').detach().appendTo('body')})
+
   $('#wechat-share-modal').on('show.bs.modal', function (e) {
     let canvas = document.getElementById('wechat-qrcode-canvas');
     let url = window.location.href;
